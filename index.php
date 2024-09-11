9de659b681863587d1e5f988af7eeeb36409a936
marcoscess <marcos.ces93@hotmail.com>
2024-09-11 20:07:01 -0300

    Estoy probando commit 1

---
+++ index.php
@@ -0,0 +1,374 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Panini Bar</title>
+                    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+         name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+         />
+            
+                    <!-- Bootstrap CSS v5.2.1 -->
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    crossorigin="anonymous"
+/>
+
+
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+/>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+<link rel="stylesheet" href="css/index.css">
+
+
+
+
+</head>
+            
+    <body>
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <div class="container">
+            <a class="navbar-brand" href="index.php"><i class="fas fa-utensils"></i> Panini Bar</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls = "navbarNav"
+            aria-expanded = "false"
+            aria-label = "Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+                
+            <div class="collapse navbar-collapse" id="navbarNav">
+
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="secciones/Admin/login.php">Inicio de sesion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" target="_blank" href="secciones/menu/menu.php">Menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="secciones/Reservas/index.php">Mesas</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimonios">Testimonios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#comentario">Contacto</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#horario">Horarios</a>
+                    </li>
+                    </ul>
+            </div>
+    </div>
+    </nav>
+
+    <section id="banner" class="container-fluid p-0">
+
+    <div class="banner-img" style="position:relative; background:url('Imagenes/fondo_login3.jpg') 
+    center/cover no-repeat; height: 400px;">
+        <div class="banner-text" style ="position:absolute; top:50%; left:50%; transform: translate(-50%, -50%); text-align:center; color:#fff">
+            <div class="logo_index">
+            <img src="imagenes/logo_index.png">
+            </div>
+            <br>
+            <a href="secciones/Menu/menu.php" class="btn btn-primary"><i class="bi bi-bag"></i> Ver Menu </a>
+        </div>
+
+    </div>
+    </section>
+
+    <seccion id="id" class="container mt-4 text-center"> 
+        
+            <div class="jumbotron bg-dark text-white ">
+                <h2>¡Bienvenido al restaurante Panini Bar!</h2>
+                    <p>
+                        Descubre una experiencia de sabor unico
+                    </p>  
+            </div>
+
+          
+        
+            <div class="card-body bg-dark ">
+                 <h5 class="card-text text-light "> ¿Quienes Somos? </h5>
+            </div>
+            <div class="card-footer text-light">
+                                
+                <p><strong>  En Panini Bar, fusionamos la tradición italiana con un toque contemporáneo para ofrecerte preparados con ingredientes frescos y cuidadosamente seleccionados. <br>
+                Desde clásicos auténticos hasta creaciones innovadoras. <br>
+                Nuestro ambiente cálido y acogedor te invita a relajarte y disfrutar de una comida memorable con amigos y familiares. <br>
+                En Panini Bar encontrarás el lugar perfecto para satisfacer tu apetito. </strong></p>
+            </div>
+                    
+    </seccion>
+   
+  
+    
+    <!-- Chefs -->
+    <section id="chefs" class="container mt-4 text-center">
+        <div class="jumbtron bg-dark text-light">
+        <h2> Nuestros Chefs </h2>
+        </div>
+        <div class="row">
+            <div class ="col-md-4">
+                <div class="card"> 
+
+                    <img src="imagenes/Personal/chef_1.jpg"
+                        class="card-img-top"
+                        alt="Chef 1"/>
+
+                    <div class="card-body bg-dark text-light">
+                        <h5 class="card-title">Chef 1 </h5>
+                        <p class="card-text"> Chef Juan Perez </p>
+                    </div>
+                    <div class="social-icons mt-3">
+                        <a href="" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                        <a href="" class="text-dark me-2"><i class="fab fa-twitter"></i></a>
+                        <a href="" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+
+                    </div>
+                </div>
+            </div>
+            <div class ="col-md-4">
+                <div class="card"> 
+
+                    <img src="imagenes/Personal/chef_3.jpg"
+                        class="card-img-top"
+                        alt="Chef 2"/>
+
+                    <div class="card-body bg-dark text-light">
+                        <h5 class="card-title">Chef 2 </h5>
+                        <p class="card-text"> Chef Sabrina </p>
+                    </div>
+                    <div class="social-icons mt-3">
+                        <a href="" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                        <a href="" class="text-dark me-2"><i class="fab fa-twitter"></i></a>
+                        <a href="" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+
+                    </div>
+                </div>
+            </div>
+            <div class ="col-md-4">
+                <div class="card"> 
+
+                    <img src="imagenes/Personal/chef_2.jpg"
+                        class="card-img-top"
+                        alt="Chef 3"/>
+
+                    <div class="card-body bg-dark text-light">
+                        <h5 class="card-title">Chef 3 </h5>
+                        <p class="card-text"> Chef Marcelo </p>
+                    </div>
+                    <div class="social-icons mt-3">
+                        <a href="" class="text-dark me-2"><i class="fab fa-facebook"></i></a>
+                        <a href="" class="text-dark me-2"><i class="fab fa-twitter"></i></a>
+                        <a href="" class="text-dark me-2"><i class="fab fa-instagram"></i></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </section>
+
+
+
+    <!--Casilla de comentarios -->
+
+    <section id="testimonios" class=" py-5">
+
+        <div class="container">
+
+        <div class="jumbtron bg-dark text-light text-center">
+        <h2> Casilla de comentarios </h2>
+        
+        </div>
+            <div class="row ">
+                
+                <div class="col-md-6 d-flex ">
+                    <div class="card mb-4 w-100 ">
+                        <div class="card-body ">
+                            <p class="card-text "> Buena Comida </p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Juan Perez
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 d-flex">
+                    <div class="card mb-4 w-100">
+                        <div class="card-body">
+                            <p class="card-text"> Muy Buena Comida </p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            Juan Perez
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+
+<!-- menu -->
+    <section class="container mt-4">
+        <div class="jumbtron bg-dark text-light text-center">
+        <h2> Menu Destacados </h2>
+        </div>
+        
+        <br>
+        <div class="row row-cols-1 row-cols-md-4 g-4">
+
+            <div class="col">
+                    <div class="card">
+                        <img src="imagenes/Menu/comida_1.jpg" alt="Haburguesa Bacon" class="card-img-top">
+                        <div class="card-body ">
+                            <h5 class="card-title"> Hamburguesa gourmet bacon chesse</h5>
+                            <p class="card-text small"><strong>Ingredientes:</strong> Carne seleccionada y procesada, queso cheddar,
+                        panceta ahumada, mayonesa, pan de papa, lechuga, tomate. Incluye guanicion de papas fritas</p>
+                            <p class="card-text"> <strong> Precio: $5100 </strong> </p>
+                        </div>
+                    </div>
+            </div>   
+            <div class="col">
+                    <div class="card">
+                        <img src="imagenes/Menu/comida_3.jpg" alt="biffe de chorizo" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title"> Biffe de chorizo grillado</h5>
+                            <p class="card-text small"><strong>Ingredientes:</strong> Carne seleccionada y procesada, queso cheddar,
+                        panceta ahumada, mayonesa, pan de papa, lechuga, tomate. Incluye guanicion de papas fritas</p>
+                            <p class="card-text"> <strong> Precio: $7500 </strong> </p>
+                        </div>
+                    </div>
+            </div> 
+            <div class="col">
+                    <div class="card">
+                        <img src="imagenes/Menu/comida_4.jpg" alt="milanesa al plato" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title"> Milanesa al plato con guarnicion</h5>
+                            <p class="card-text small"><strong>Ingredientes:</strong> Carne seleccionada y procesada, queso cheddar,
+                        panceta ahumada, mayonesa, pan de papa, lechuga, tomate. Incluye guanicion de papas fritas</p>
+                            <p class="card-text"> <strong> Precio: $7000 </strong> </p>
+                        </div>
+                    </div>
+            </div> 
+            <div class="col">
+                    <div class="card">
+                        <img src="imagenes/Menu/carre-de-cordeiro_5.jpg" alt="carre de cordero" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title"> Carre de cordero grillado</h5>
+                            <p class="card-text small"><strong>Ingredientes:</strong> Carne seleccionada y procesada, queso cheddar,
+                        panceta ahumada, mayonesa, pan de papa, lechuga, tomate. Incluye guanicion de papas fritas</p>
+                            <p class="card-text"> <strong> Precio: $8000 </strong> </p>
+                        </div>
+                    </div>
+            </div>   
+        </div>
+    </section>
+    <br>
+    <br>
+
+    <!--Contacto -->
+    <section id="comentario" class="container mt-4">
+    <div class="jumbtron text-light">
+        <h2> Comentarios </h2>
+        <p>Estamos para servirle</p>
+        </div> 
+               
+        
+        <form action="?" method="post"> 
+            <div class="mb-3 text-light">
+                <label for="name"> Nombre: </label>
+                <br>
+                <input
+                    class="form-control"
+                    type="text"
+                    name="nombre"
+                    placeholder="Ingrese su nombre"
+                    required
+                >
+            </div>
+        
+            <div class="mb-3 text-light">
+                <label for="correo"> Correo </label>
+                <br>
+                <input
+                    class="form-control"
+                    type="email"
+                    name="correo"
+                    placeholder="Ingrese su correo"
+                    required
+                />
+            </div>    
+            
+            <div class="mb-3 text-light">
+                <label for="message">Mensaje: </label>
+                <br>
+                <textarea
+                    class="form-control"
+                    required
+                    name="mensaje"
+                    id="message"
+                    rows="6"
+                    cols="50"
+                    placeholder="Escriba su mensaje...">
+                </textarea>
+                <br>
+                <input type="submit" class="btn btn-primary" value="Enviar mensaje">
+            </div>
+         </form>
+    </section>
+    <br><br>
+
+    <!-- horarios -->
+    <div id="horario" class="text-center text-light p-4">
+        <h3 class="mb-4 bg-dark"> Horarios de atencion </h3>
+        <div>
+            <p> <strong>Martes a Viernes </strong></p>
+            <p> <strong>Mañana: 09:00 am - 15:00 pm </strong></p>
+            <p> <strong>Tarde: 20:00 pm - 01:00 am </strong></p>
+        </div>
+        <div>
+            <p> <strong>Sabado y Domingo </strong></p>
+            <p> <strong>Mañana: 09:00 am - 15:00 pm </strong></p>
+            <p> <strong>Tarde: 20:00 pm - 03:00 am </strong></p>
+        </div>
+
+    </div>
+    
+
+
+    <footer class="bg-dark text-light text-center p-4">
+        <p> &copy;Panini Bar 2023 </p>
+        
+                        
+    </footer>
+                    <!-- Bootstrap JavaScript Libraries -->
+    <script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossorigin="anonymous"
+        ></script>
+            
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+        integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+        crossorigin="anonymous"
+        ></script>
+    
+    </body>
+</html>
+            
